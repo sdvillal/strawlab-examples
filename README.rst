390523255b39bcee9192eac7a5e4379dd902da57
santi <sdvillal@gmail.com>
2015-08-29 07:55:50 +0100

    installable wannabe

---
+++ README.rst
@@ -0,0 +1,8 @@
+Strawlab example analysis
+=========================
+
+Just clone and install with pip; read and run *strawlab_examples/euroscipy/euroscipy_example.py*,
+which will download a sample dataset (1.7GB) and perform some analysis.
+
+Note that for running the full benchmarks you would need datasets that are unreleased yet.
+Use the public dataset (neuropeptide degradation) to get a feeling on how these work.
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2
+# coding=utf-8
+
+# Authors: Santi Villalba <sdvillal@gmail.com>
+# Licence: BSD 3 clause
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='strawlab_examples',
+    license='BSD 3 clause',
+    description='Data analysis examples from the Straw lab',
+    long_description=open('README.rst').read().replace('|Build Status| |Coverage Status| |Scrutinizer Status|', ''),
+    version='1.0.0-dev0',
+    url='https://github.com/strawlab/strawlab-examples',
+    author='Santi Villalba',
+    author_email='sdvillal@gmail.com',
+    packages=['strawlab_examples',
+              'strawlab_examples.benchmarks',
+              'strawlab_examples.euroscipy'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: Unix',
+    ],
+    install_requires=['whatami', 'jagged',  'arpeggio>=1.0', 'future', 'humanize'],
+    tests_require=['pytest', 'pytest-cov', 'pytest-pep8'],
+    platforms=['Any'],
+)
